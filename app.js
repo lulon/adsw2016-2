a448ad499391ac25d137800e4d39f5ae581f5524
Saulo Bernal Moreira <s.bernal.sak@gmail.com>
2016-09-11 20:14:54 -0400

    App and package update

--- app.js
+++ app.js
@@ -3,22 +3,27 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
-//load customers route
 var contact = require('./routes/contact'); 
+var users = require('./routes/users');
+var admin = require('./routes/admin')
 var app = express();
+var flash = require('connect-flash');
 
 var connection  = require('express-myconnection'); 
 var mysql = require('mysql');
+
+var isUserLogged = false;
+var isAdminLogged = false;
 
 // all environments
 app.set('port', process.env.PORT || 4300);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(flash());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,6 +31,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+app.use(isUserLogged);
+app.use(isAdminLogged);
 
 app.use(
     
@@ -47,13 +54,25 @@ app.get('/', routes.index);
 app.get('/contact', contact.list);
 app.get('/contact/add', contact.add);
 app.post('/contact/add', contact.save);
-app.get('/contact/delete/:id', contact.delete_customer);
-app.get('/contact/edit/:id', contact.edit);
-app.post('/contact/edit/:id',contact.save_edit);
-
+app.get('/contact/delete/:phone', contact.delete_customer);
+app.get('/contact/edit/:phone', contact.edit);
+app.post('/contact/edit/:phone',contact.save_edit);
+app.get('/user', admin.list);
+app.get('/user/add', admin.add);
+app.post('/user/add', admin.save);
+app.get('/user/delete/:username', admin.delete_customer);
+app.get('/user/edit/:username', admin.edit);
+app.post('/user/edit/:username',admin.save_edit);
+app.get('/user_logout', users.user_logout);
+app.get('/admin_logout', users.admin_logout);
+app.get('/user_login', users.user_login);
+app.get('/admin_login', users.admin_login);
+app.get('/bad_login', users.bad_login);
+app.post('/admin_login_handler', users.admin_login_handler);
+app.post('/user_login_handler', users.user_login_handler);
 
 app.use(app.router);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('The game starts on port ' + app.get('port'));
 });
