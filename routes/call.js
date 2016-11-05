68a5b8e4dd68d0f3a4a0676bf6e99d5a3b7f4631
Saulo Bernal Moreira <s.bernal.sak@gmail.com>
2016-11-05 19:02:54 -0300

    Call.js update and ejs on fucking progress bitches

--- routes/call.js
+++ routes/call.js
@@ -2,42 +2,40 @@
 exports.start_surveys = function(req, res){
 	if(req.session.isUserLogged){
 
+		var proyect = req.params.proyectname;
+
 		if(typeof req.session.quizes == 'undefined'){
 
 		    req.getConnection(function (err, connection) {
 
-		    	var proyect = req.params.proyectname;
-
-		        connection.query("SELECT link FROM quiz WHERE idproyect = ?",[proyect], function(err, rows)
+		        connection.query("SELECT * FROM quiz WHERE idproyect = ?",[proyect], function(err, rows)
 		        {
-		    
 		            if(err)
-		                console.log("Error deleting : %s ", err);
+		                console.log("Error : %s ", err);
 		            
-		            req.session.quizes = rows;
-
-		            res.redirect('/contact');
-		             
+		            req.session.quizes = rows;		             
 		        });
 		        
 		    });			
 		}
 
+		var rand = Math.floor(Math.random() * req.session.quizes.length);
 
+		req.session.selected_number = rand;
 
-	    req.getConnection(function (err, connection) {
-	        
-	        connection.query("SELECT FROM quiz INNER JOIN proyect ON  ",[phone], function(err, rows)
-	        {
-	            
-	             if(err)
-	                 console.log("Error deleting : %s ",err );
-	            
-	             res.redirect('/contact');
-	             
-	        });
-	        
-	    });
+		req.getConnection(function (err, connection) {
+
+			connection.query("SELECT * FROM contact WHERE NOT EXIST (SELECT * FROM call WHERE idquiz = ?)", [req.session.quizes[rand].idquiz], function(err, rows)
+				{
+					if(err)
+		                console.log("Error : %s ", err);
+
+		            res.render('call',{page_title:"Call",data:rows});
+
+				})
+
+		});
+
 	}
 	else res.redirect('/bad_login');
 }
